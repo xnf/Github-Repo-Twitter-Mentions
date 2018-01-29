3c1d0c8b3ad5dc878d516f78bb9e3a6301d482d3
Edgars Zagorskis <edgars.zagorskis@gmail.com>
2018-01-30 01:00:38 +0200

    Add recursive tweet search for repos

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+const chalk = require('chalk');
 const logger = require('./src/logger');
 const init = require('./src/init');
 const githubService = require('./src/github.service');
 const twitterService = require('./src/twitter.service');
+const _ = require('lodash');
 
-const getTweetsForThisRepo = async () => {
-  const tweets = await twitterService.searchTweets('https://github.com/angular');
-  // const tweets = await twitterService.user('winchaclub');
-  logger.info(tweets);
+const getTweetsForThisRepo = async (repo) => {
+  const repoTitle = `${chalk.bgBlackBright(repo.name)} by ${chalk.bgBlackBright(repo.owner.login)}`;
+  const tweets = await twitterService.searchTweets(repo.url);
+  if (_.isEmpty(tweets.statuses)) {
+    logger.info(`\tNobody talks about ${repoTitle}`);
+  } else {
+    logger.info(`\tLook who is talking about ${repoTitle}`);
+    tweets.statuses.forEach(tweet => logger.info(tweet));
+  }
 };
 
-const getTweetsForRepos = (topic) => {
-  const repos = githubService.findReposByTopic(topic);
+const getTweetsForRepos = async (topic, count) => {
+  logger.debug(`Looking for ${count} repos and ${count} tweets on topic '${chalk.bgBlackBright(topic)}'`);
+  (await githubService.findReposByTopic(topic, count)).forEach(repo => getTweetsForThisRepo(repo));
 };
 
 try {
@@ -19,50 +27,3 @@ try {
 } catch (e) {
   logger.error(e);
 }
-
-// getTweetsForThisRepo();
-
-// setTimeout(()=>{
-//     logger.info('a');
-//
-//     setTimeout(()=>{
-//         logger.info('b');
-//     },1000);
-// },1000);
-
-
-// /**
-//  * To install dependencies, do
-//  * npm i request request-promise-native
-//  */
-// const rp = require('request-promise-native');
-//
-// const SEARCH_URL_BASE = 'https://jsonmock.hackerrank.com/api/movies/search/?Title=';
-//
-// function getMovieTitles(substr) {
-//     start(substr).then(movieTitles => {
-//         movieTitles.forEach(movieTitle => console.log(movieTitle));
-// });
-// }
-//
-// const start = async (term) => {
-//     const store = [];
-//     const initialData = await fetchRemoteData(term);
-//     parseAndStore(store, initialData);
-//     while (initialData.page++ < initialData.total_pages) {
-//         parseAndStore(store, await fetchRemoteData(term, initialData.page));
-//     }
-//     store.sort();
-//     return store;
-// };
-//
-// const parseAndStore = (store, response) => {
-//     response.data.forEach(movie => store.push(movie.Title));
-// };
-//
-//
-// const fetchRemoteData = async (term, page = 1) => {
-//     return JSON.parse(await rp(`${SEARCH_URL_BASE}${term}&page=${page}`));
-// };
-//
-// getMovieTitles('Spiderman');
--- src/github.service.js
+++ src/github.service.js
@@ -2,8 +2,18 @@ const request = require('request-promise-native');
 
 const BASE_URL = 'https://api.github.com/search/repositories';
 
+const makeRequestObject = url => ({
+  url,
+  headers: {
+    'User-Agent': 'NodeJS/Request',
+  },
+});
+
 const service = {
-  findReposByTopic: async (title, limit = 10) => request(`${BASE_URL}?q=topic:${title}&per_page=${limit}`),
+  findReposByTopic: async (title, limit = 10) => {
+    const response = await request(makeRequestObject(`${BASE_URL}?q=topic:${title}&per_page=${limit}`));
+    return JSON.parse(response).items;
+  },
 };
 
 module.exports = service;
